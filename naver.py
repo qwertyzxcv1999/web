ec679c3607c260200c9caadd48761c763deab0d6
unknown <anjrmfsjrjwl@naver.com>
2019-05-01 20:37:02 +0900

    <>

--- naver.py
+++ naver.py
@@ -2,4 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 req = requests.get('https://www.naver.com/')
-print(req.text)
+soup=BeautifulSoup(req.text,'html.parser') 
+s=soup.find_all("span",{'class':'ah_k'})
+a=1
+for i in s:
+
+    print(a,"위",i.text)
+    a=a+1
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello")
+from bs4 import BeautifulSoup
+
+import requests
+
+# web_url에 원하는 웹의 URL을 넣어주시면 됩니다.
+r = requests.get(https://www.naver.com/)
+r.status_code
+200
+r.headers['content-type']
+'text/html; charset=UTF-8'
+r.encoding
+'UTF-8'
+r.text
+<!DOCTYPE html>
+<html class="client-nojs" lang="en" dir="ltr">
