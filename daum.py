357cd5f000fe60bef14980a1f5c6be57b0f44c4c
unknown <anjrmfsjrjwl@naver.com>
2019-05-11 16:54:02 +0900

    이게이번에한다음검색어

--- daum.py
+++ daum.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 
 req = requests.get('https://www.daum.net/')
 soup=BeautifulSoup(req.text,'html.parser') 
-s=soup.select(" ol.list_hotissue > li > div")[1].select("span")[1]
-
 a=1
-for i in s.findAll('span'):
-    print(i.text)
+s=soup.select('.hotissue_mini a[class*=link_issue]')
+for i in s:
+    print(a,"위",i.text)
     a=a+1
-
-
     
+    #mArticle > div.cmain_tmp > div.section_media > div.hot_issue.issue_mini > div.hotissue_layer > ol > li:nth-child(3) > div > div:nth-child(1) > span.txt_issue > a
+    #mArticle > div.cmain_tmp > div.section_media > div.hotissue_builtin > div.realtime_part > ol > li:nth-child(1) > div > div:nth-child(1) > span.txt_issue > a
+    #mArticle > div.cmain_tmp > div.section_media > div.hot_issue.issue_mini > div.hotissue_layer > ol > li:nth-child(3) > div > div:nth-child(1) > span.txt_issue > a
+    #mArticle > div.cmain_tmp > div.section_media > div.hot_issue.issue_mini.hide > div.hotissue_layer > ol > li:nth-child(3) > div > div:nth-child(1) > span.txt_issue > a
